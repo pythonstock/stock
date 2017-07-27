35650b824b12f33604075f77ebd7a5c83aa3cd7d
yhy <yhy>
2017-07-27 11:40:37 +0800

    修改 shel fi

--- docker/startStock.sh
+++ docker/startStock.sh
@@ -21,8 +21,9 @@ if [ $DB_IS_RUN -ne 2 ]; then
     sh startMysql.sh
 fi
 
+#检查stock启动
 STOCK_IS_RUN=`docker ps --filter "name=stock" --filter "status=running" | wc -l `
-if [ STOCK_IS_RUN -ne 2 ]; then
+if [ $STOCK_IS_RUN -ne 2 ]; then
     # /data/stock 是代码目录 -v /data/stock:/data/stock 是开发模式。
     docker run -itd --link=mariadb --name stock  \
         -p 8888:8888 \
@@ -30,4 +31,6 @@ if [ STOCK_IS_RUN -ne 2 ]; then
         -p 9999:9999 \
        github.com/pythonstock/stock/tensorflow-py3-stock:latest
     echo "starting stock ..."
+else
+    echo "stock is running !!!"
 fi
