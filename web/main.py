1a215c9520adf320bc6fbcec577ab92b9f3cd6fc
yhy <yhy>
2017-07-27 13:07:01 +0800

    增加页面。启动。

---
+++ web/main.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+#
+# Copyright 2009 Facebook
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import bcrypt
+import concurrent.futures
+import MySQLdb
+import markdown
+import os.path
+import re
+import subprocess
+import torndb
+import tornado.escape
+from tornado import gen
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import libs.common as common
+
+# A thread pool to be used for password hashing with bcrypt.
+executor = concurrent.futures.ThreadPoolExecutor(2)
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", HomeHandler),
+        ]
+        settings = dict(
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            xsrf_cookies=True,
+            #RANDOM_VALUE
+            cookie_secret="fc03ad14f21cf81867cbed33109027bb1b670eddf0392cdda8709268a17b58b7",
+            debug=True,
+        )
+        super(Application, self).__init__(handlers, **settings)
+        # Have one global connection to the blog DB across all handlers
+        self.db = torndb.Connection(
+            host=common.MYSQL_HOST, database=common.MYSQL_DB,
+            user=common.MYSQL_USER, password=common.MYSQL_PWD)
+
+class BaseHandler(tornado.web.RequestHandler):
+    @property
+    def db(self):
+        return self.application.db
+
+class HomeHandler(BaseHandler):
+    def get(self):
+        self.render("index.html", entries="hello")
+
+def main():
+    tornado.options.parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(Application())
+    port = 9999
+    http_server.listen(port)
+    tornado.ioloop.IOLoop.current().start()
+
+if __name__ == "__main__":
+    main()
