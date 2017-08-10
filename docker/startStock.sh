91b443b84dd437833915bd512a2f1099569c19f9
yhy <yhy>
2017-08-10 10:43:52 +0800

    判断如果 存在 stock 就重启 stock。重新部署。

--- docker/startStock.sh
+++ docker/startStock.sh
@@ -23,14 +23,17 @@ fi
 
 #检查stock启动
 STOCK_IS_RUN=`docker ps --filter "name=stock" --filter "status=running" | wc -l `
-if [ $STOCK_IS_RUN -ne 2 ]; then
-    # /data/stock 是代码目录 -v /data/stock:/data/stock 是开发模式。
-    docker run -itd --link=mariadb --name stock  \
-        -p 8888:8888 \
-        -p 6006:6006 \
-        -p 9999:9999 \
-       github.com/pythonstock/stock/tensorflow-py3-stock:latest
-    echo "starting stock ..."
-else
-    echo "stock is running !!!"
+if [ $STOCK_IS_RUN -ge 2 ]; then
+    docker stop stock && docker rm stock
 fi
+
+sleep 1
+
+# /data/stock 是代码目录 -v /data/stock:/data/stock 是开发模式。
+docker run -itd --link=mariadb --name stock  \
+    -v /Users/test/PyWorkspace/stock-github:/data/stock \
+    -p 8888:8888 \
+    -p 6006:6006 \
+    -p 9999:9999 \
+   github.com/pythonstock/stock/tensorflow-py3-stock:latest
+echo "starting stock ..."
