ebe23a651d6c7ebcdb4d379d00cba9b593920b7f
yhy <yhy>
2017-08-10 10:47:16 +0800

    增加创建端口。

--- docker/startStock.sh
+++ docker/startStock.sh
@@ -33,7 +33,6 @@ sleep 1
 echo "starting stock ..."
 # /data/stock 是代码目录 -v /data/stock:/data/stock 是开发模式。
 docker run -itd --link=mariadb --name stock  \
-    -v /Users/test/PyWorkspace/stock-github:/data/stock \
     -p 8888:8888 \
     -p 6006:6006 \
     -p 9999:9999 \
