91adda96cd7a2fb94254c32771847fff37ccbb30
yhy <yhy>
2017-09-19 11:03:26 +0800

    增加 search 查询。

--- web/main.py
+++ web/main.py
@@ -30,6 +30,7 @@ import tornado.options
 import tornado.web
 import libs.common as common
 import libs.stock_web_dic as stock_web_dic
+import logging
 
 # A thread pool to be used for password hashing with bcrypt.
 executor = concurrent.futures.ThreadPoolExecutor(2)
@@ -100,19 +101,6 @@ class GetStockHtmlHandler(BaseHandler):
 # 获得股票数据内容。
 class GetStockDataHandler(BaseHandler):
     def get(self):
-        # https://datatables.net/manual/server-side
-        self.set_header('Content-Type', 'application/json;charset=UTF-8')
-        order_by_column = []
-        order_by_dir = []
-        # 支持多排序。使用shift+鼠标左键。
-        for item, val in self.request.arguments.items():
-            # print("order:", item)
-            if str(item).startswith("order["):
-                print("order:", item, ",val:", val[0])
-            if str(item).startswith("order[") and str(item).endswith("[column]"):
-                order_by_column.append(int(val[0]))
-            if str(item).startswith("order[") and str(item).endswith("[dir]"):
-                order_by_dir.append(val[0].decode("utf-8"))  # bytes转换字符串
 
         # 获得分页参数。
         start_param = self.get_argument("start", default=0, strip=False)
@@ -121,6 +109,51 @@ class GetStockDataHandler(BaseHandler):
 
         name_param = self.get_argument("name", default=None, strip=False)
         stock_web = stock_web_dic.STOCK_WEB_DATA_MAP[name_param]
+
+        # https://datatables.net/manual/server-side
+        self.set_header('Content-Type', 'application/json;charset=UTF-8')
+        order_by_column = []
+        order_by_dir = []
+        # 支持多排序。使用shift+鼠标左键。
+        for item, val in self.request.arguments.items():
+            # logging.info("item: %s, val: %s" % (item, val) )
+            if str(item).startswith("order["):
+                print("order:", item, ",val:", val[0])
+            if str(item).startswith("order[") and str(item).endswith("[column]"):
+                order_by_column.append(int(val[0]))
+            if str(item).startswith("order[") and str(item).endswith("[dir]"):
+                order_by_dir.append(val[0].decode("utf-8"))  # bytes转换字符串
+
+        search_by_column = []
+        search_by_data = []
+
+        # 返回search字段。
+        for item, val in self.request.arguments.items():
+            # logging.info("item: %s, val: %s" % (item, val))
+            if str(item).startswith("columns[") and str(item).endswith("[search][value]"):
+                logging.info("item: %s, val: %s" % (item, val))
+                str_idx = item.replace("columns[", "").replace("][search][value]", "")
+                int_idx = int(str_idx)
+                # 找到字符串
+                str_val = val[0].decode("utf-8")
+                if str_val != "":  # 字符串。
+                    search_by_column.append(stock_web.columns[int_idx])
+                    search_by_data.append(val[0].decode("utf-8"))  # bytes转换字符串
+
+        # 打印日志。
+        search_sql = ""
+        search_idx = 0
+        logging.info(search_by_column)
+        logging.info(search_by_data)
+        for item in search_by_column:
+            val = search_by_data[search_idx]
+            logging.info("idx: %s, column: %s, value: %s " % (search_idx, item, val))
+            # 查询sql
+            if search_idx == 0:
+                search_sql = " WHERE %s = '%s' " % (item, val)
+            else:
+                search_sql = search_sql + " AND %s = '%s' " % (item, val)
+            search_idx = search_idx + 1
 
         # print("stockWeb :", stock_web)
         order_by_sql = ""
@@ -138,11 +171,15 @@ class GetStockDataHandler(BaseHandler):
                     order_by_sql += " cast(%s as double) %s" % (col_tmp, dir_tmp)
                 idx += 1
         # 查询数据库。
-        sql = " SELECT * FROM %s %s LIMIT %s,%s " % (stock_web.table_name, order_by_sql, start_param, length_param)
-        print("select sql :", sql)
+        sql = " SELECT * FROM %s %s %s LIMIT %s , %s " % (
+            stock_web.table_name, search_sql, order_by_sql, start_param, length_param)
+        count_sql = " SELECT count(1) as num FROM %s %s " % (stock_web.table_name, search_sql)
+
+        logging.info("select sql : " + sql)
+        logging.info("count sql : " + count_sql)
         stock_web_list = self.db.query(sql)
-        stock_web_size = self.db.query(" SELECT count(1) as num FROM " + stock_web.table_name)
-        print("stockWebList size :", stock_web_size)
+        stock_web_size = self.db.query(count_sql)
+        logging.info("stockWebList size : %s " % stock_web_size)
 
         obj = {
             "draw": 0,
