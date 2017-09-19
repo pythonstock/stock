919c8aa7dbd4ae013fd7a26ea1689cad91de632d
yhy <yhy>
2017-09-19 09:13:57 +0800

    使用 python 3.

--- jobs/run_init.sh
+++ jobs/run_init.sh
@@ -5,4 +5,4 @@ DATE=`date +%Y-%m-%d:%H:%M:%S`
 
 echo $DATE >> /data/logs/run_init.log
 
-/usr/bin/python3.5 /data/stock/jobs/basic_job.py  >> /data/logs/run_init.log
+/usr/bin/python3 /data/stock/jobs/basic_job.py  >> /data/logs/run_init.log
