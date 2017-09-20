0eabc34b83ef8f2fe064b74c6ced6b30113bfc0f
yhy <yhy>
2017-09-20 11:23:35 +0800

    拆分 handler 模块，分成 dataTable 模块，chat 模块。

---
+++ web/dataTableHandler.py
@@ -0,0 +1,122 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+
+import bcrypt
+import MySQLdb
+import markdown
+import os.path
+import json
+import subprocess
+import torndb
+import tornado.escape
+from tornado import gen
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import libs.stock_web_dic as stock_web_dic
+import web.base as webBase
+import logging
+
+
+# 获得页面数据。
+class GetStockHtmlHandler(webBase.BaseHandler):
+    @gen.coroutine
+    def get(self):
+        name = self.get_argument("name", default=None, strip=False)
+        stockWeb = stock_web_dic.STOCK_WEB_DATA_MAP[name]
+        # self.uri_ = ("self.request.url:", self.request.uri)
+        # print self.uri_
+        self.render("stock_web.html", stockWeb=stockWeb, leftMenu=webBase.GetLeftMenu(self.request.uri))
+
+
+# 获得股票数据内容。
+class GetStockDataHandler(webBase.BaseHandler):
+    def get(self):
+
+        # 获得分页参数。
+        start_param = self.get_argument("start", default=0, strip=False)
+        length_param = self.get_argument("length", default=10, strip=False)
+        print("page param:", length_param, start_param)
+
+        name_param = self.get_argument("name", default=None, strip=False)
+        stock_web = stock_web_dic.STOCK_WEB_DATA_MAP[name_param]
+
+        # https://datatables.net/manual/server-side
+        self.set_header('Content-Type', 'application/json;charset=UTF-8')
+        order_by_column = []
+        order_by_dir = []
+        # 支持多排序。使用shift+鼠标左键。
+        for item, val in self.request.arguments.items():
+            # logging.info("item: %s, val: %s" % (item, val) )
+            if str(item).startswith("order["):
+                print("order:", item, ",val:", val[0])
+            if str(item).startswith("order[") and str(item).endswith("[column]"):
+                order_by_column.append(int(val[0]))
+            if str(item).startswith("order[") and str(item).endswith("[dir]"):
+                order_by_dir.append(val[0].decode("utf-8"))  # bytes转换字符串
+
+        search_by_column = []
+        search_by_data = []
+
+        # 返回search字段。
+        for item, val in self.request.arguments.items():
+            # logging.info("item: %s, val: %s" % (item, val))
+            if str(item).startswith("columns[") and str(item).endswith("[search][value]"):
+                logging.info("item: %s, val: %s" % (item, val))
+                str_idx = item.replace("columns[", "").replace("][search][value]", "")
+                int_idx = int(str_idx)
+                # 找到字符串
+                str_val = val[0].decode("utf-8")
+                if str_val != "":  # 字符串。
+                    search_by_column.append(stock_web.columns[int_idx])
+                    search_by_data.append(val[0].decode("utf-8"))  # bytes转换字符串
+
+        # 打印日志。
+        search_sql = ""
+        search_idx = 0
+        logging.info(search_by_column)
+        logging.info(search_by_data)
+        for item in search_by_column:
+            val = search_by_data[search_idx]
+            logging.info("idx: %s, column: %s, value: %s " % (search_idx, item, val))
+            # 查询sql
+            if search_idx == 0:
+                search_sql = " WHERE `%s` = '%s' " % (item, val)
+            else:
+                search_sql = search_sql + " AND `%s` = '%s' " % (item, val)
+            search_idx = search_idx + 1
+
+        # print("stockWeb :", stock_web)
+        order_by_sql = ""
+        # 增加排序。
+        if len(order_by_column) != 0 and len(order_by_dir) != 0:
+            order_by_sql = "  ORDER BY "
+            idx = 0
+            for key in order_by_column:
+                # 找到排序字段和dir。
+                col_tmp = stock_web.columns[key]
+                dir_tmp = order_by_dir[idx]
+                if idx != 0:
+                    order_by_sql += " ,cast(%s as double) %s" % (col_tmp, dir_tmp)
+                else:
+                    order_by_sql += " cast(%s as double) %s" % (col_tmp, dir_tmp)
+                idx += 1
+        # 查询数据库。
+        sql = " SELECT * FROM `%s` %s %s LIMIT %s , %s " % (
+            stock_web.table_name, search_sql, order_by_sql, start_param, length_param)
+        count_sql = " SELECT count(1) as num FROM `%s` %s " % (stock_web.table_name, search_sql)
+
+        logging.info("select sql : " + sql)
+        logging.info("count sql : " + count_sql)
+        stock_web_list = self.db.query(sql)
+        stock_web_size = self.db.query(count_sql)
+        logging.info("stockWebList size : %s " % stock_web_size)
+
+        obj = {
+            "draw": 0,
+            "recordsTotal": stock_web_size[0]["num"],
+            "recordsFiltered": stock_web_size[0]["num"],
+            "data": stock_web_list
+        }
+        self.write(json.dumps(obj))
--- web/main.py
+++ web/main.py
@@ -1,18 +1,5 @@
-#!/usr/bin/env python
-#
-# Copyright 2009 Facebook
-#
-# Licensed under the Apache License, Version 2.0 (the "License"); you may
-# not use this file except in compliance with the License. You may obtain
-# a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations
-# under the License.
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
 
 import bcrypt
 import concurrent.futures
@@ -30,6 +17,8 @@ import tornado.options
 import tornado.web
 import libs.common as common
 import libs.stock_web_dic as stock_web_dic
+import web.dataTableHandler as dataTableHandler
+import web.base as webBase
 import logging
 
 # A thread pool to be used for password hashing with bcrypt.
@@ -38,10 +27,12 @@ executor = concurrent.futures.ThreadPoolExecutor(2)
 
 class Application(tornado.web.Application):
     def __init__(self):
-        handlers = [  # 设置路由
+        handlers = [
+            # 设置路由
             (r"/", HomeHandler),
-            (r"/stock/api_data", GetStockDataHandler),
-            (r"/stock/data", GetStockHtmlHandler),
+            # 使用datatable 展示报表数据模块。
+            (r"/stock/api_data", dataTableHandler.GetStockDataHandler),
+            (r"/stock/data", dataTableHandler.GetStockHtmlHandler),
         ]
         settings = dict(  # 配置
             template_path=os.path.join(os.path.dirname(__file__), "templates"),
@@ -58,136 +49,11 @@ class Application(tornado.web.Application):
             user=common.MYSQL_USER, password=common.MYSQL_PWD)
 
 
-class BaseHandler(tornado.web.RequestHandler):
-    @property
-    def db(self):
-        try:
-            # check every time。
-            self.application.db.query("SELECT 1 ")
-        except Exception as e:
-            print(e)
-            self.application.db.reconnect()
-        return self.application.db
-
-
-class HomeHandler(BaseHandler):
+# 首页handler。
+class HomeHandler(webBase.BaseHandler):
     @gen.coroutine
     def get(self):
-        self.render("index.html", entries="hello", leftMenu=getLeftMenu(self.request.uri))
-
-
-class LeftMenu:
-    def __init__(self, url):
-        self.leftMenuList = stock_web_dic.STOCK_WEB_DATA_LIST
-        self.current_url = url
-
-
-# 获得左菜单。
-def getLeftMenu(url):
-    return LeftMenu(url)
-
-
-# 获得页面数据。
-class GetStockHtmlHandler(BaseHandler):
-    @gen.coroutine
-    def get(self):
-        name = self.get_argument("name", default=None, strip=False)
-        stockWeb = stock_web_dic.STOCK_WEB_DATA_MAP[name]
-        # self.uri_ = ("self.request.url:", self.request.uri)
-        # print self.uri_
-        self.render("stock_web.html", stockWeb=stockWeb, leftMenu=getLeftMenu(self.request.uri))
-
-
-# 获得股票数据内容。
-class GetStockDataHandler(BaseHandler):
-    def get(self):
-
-        # 获得分页参数。
-        start_param = self.get_argument("start", default=0, strip=False)
-        length_param = self.get_argument("length", default=10, strip=False)
-        print("page param:", length_param, start_param)
-
-        name_param = self.get_argument("name", default=None, strip=False)
-        stock_web = stock_web_dic.STOCK_WEB_DATA_MAP[name_param]
-
-        # https://datatables.net/manual/server-side
-        self.set_header('Content-Type', 'application/json;charset=UTF-8')
-        order_by_column = []
-        order_by_dir = []
-        # 支持多排序。使用shift+鼠标左键。
-        for item, val in self.request.arguments.items():
-            # logging.info("item: %s, val: %s" % (item, val) )
-            if str(item).startswith("order["):
-                print("order:", item, ",val:", val[0])
-            if str(item).startswith("order[") and str(item).endswith("[column]"):
-                order_by_column.append(int(val[0]))
-            if str(item).startswith("order[") and str(item).endswith("[dir]"):
-                order_by_dir.append(val[0].decode("utf-8"))  # bytes转换字符串
-
-        search_by_column = []
-        search_by_data = []
-
-        # 返回search字段。
-        for item, val in self.request.arguments.items():
-            # logging.info("item: %s, val: %s" % (item, val))
-            if str(item).startswith("columns[") and str(item).endswith("[search][value]"):
-                logging.info("item: %s, val: %s" % (item, val))
-                str_idx = item.replace("columns[", "").replace("][search][value]", "")
-                int_idx = int(str_idx)
-                # 找到字符串
-                str_val = val[0].decode("utf-8")
-                if str_val != "":  # 字符串。
-                    search_by_column.append(stock_web.columns[int_idx])
-                    search_by_data.append(val[0].decode("utf-8"))  # bytes转换字符串
-
-        # 打印日志。
-        search_sql = ""
-        search_idx = 0
-        logging.info(search_by_column)
-        logging.info(search_by_data)
-        for item in search_by_column:
-            val = search_by_data[search_idx]
-            logging.info("idx: %s, column: %s, value: %s " % (search_idx, item, val))
-            # 查询sql
-            if search_idx == 0:
-                search_sql = " WHERE `%s` = '%s' " % (item, val)
-            else:
-                search_sql = search_sql + " AND `%s` = '%s' " % (item, val)
-            search_idx = search_idx + 1
-
-        # print("stockWeb :", stock_web)
-        order_by_sql = ""
-        # 增加排序。
-        if len(order_by_column) != 0 and len(order_by_dir) != 0:
-            order_by_sql = "  ORDER BY "
-            idx = 0
-            for key in order_by_column:
-                # 找到排序字段和dir。
-                col_tmp = stock_web.columns[key]
-                dir_tmp = order_by_dir[idx]
-                if idx != 0:
-                    order_by_sql += " ,cast(%s as double) %s" % (col_tmp, dir_tmp)
-                else:
-                    order_by_sql += " cast(%s as double) %s" % (col_tmp, dir_tmp)
-                idx += 1
-        # 查询数据库。
-        sql = " SELECT * FROM `%s` %s %s LIMIT %s , %s " % (
-            stock_web.table_name, search_sql, order_by_sql, start_param, length_param)
-        count_sql = " SELECT count(1) as num FROM `%s` %s " % (stock_web.table_name, search_sql)
-
-        logging.info("select sql : " + sql)
-        logging.info("count sql : " + count_sql)
-        stock_web_list = self.db.query(sql)
-        stock_web_size = self.db.query(count_sql)
-        logging.info("stockWebList size : %s " % stock_web_size)
-
-        obj = {
-            "draw": 0,
-            "recordsTotal": stock_web_size[0]["num"],
-            "recordsFiltered": stock_web_size[0]["num"],
-            "data": stock_web_list
-        }
-        self.write(json.dumps(obj))
+        self.render("index.html", entries="hello", leftMenu=webBase.GetLeftMenu(self.request.uri))
 
 
 def main():
