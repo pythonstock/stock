b4d86cc5ae3cd99ac753b7dd7715af6fa4fffbd3
yhy <yhy>
2017-10-27 10:13:38 +0800

    增加 30 个数据。用作演示。

--- tf/minst_serving/trans_data2img.py
+++ tf/minst_serving/trans_data2img.py
@@ -6,9 +6,10 @@ import mnist_input_data
 import numpy as np
 
 input_dir = "/data/stock/tf/minst_serving/input_data"
-out_dir = "/data/stock/web/static/img/minst_serving/%s.jpeg"
+out_dir = "/data/stock/web/static/img/minst_serving/%s.bmp"
 
 
+# https://pillow.readthedocs.io/en/3.0.0/reference/Image.html
 def trans_image(idx, img_arr):
     # https://docs.scipy.org/doc/numpy/reference/routines.math.html
     img_arr = np.add(np.multiply(255.0, img_arr), 255.0)
@@ -18,15 +19,47 @@ def trans_image(idx, img_arr):
     # imgplot = plt.imshow(img, cmap='gray') #显示灰度图像
     img_1 = Image.fromarray(img_arr)
     out_file = out_dir % idx
-    img_1.save(out_file)
+    # 必须是BMP的类型，其他类型，数据都经过压缩。造成数据不一致。
+    img_1.save(out_file, format="BMP")
+    return img_arr
+
+
+def read_image_arr(image_file):
+    img = Image.open(image_file)
+
+    img_array = np.array(img, dtype=np.uint8)
+    # print(img_array)
+    img_array = img_array.reshape(img_array.size)
+    img_array = np.divide(np.mod(img_array, 255), 255.0)
+    #img_array = np.divide(np.subtract(255.0, img_array), 255.0)
+    # print(img_array)
+    return img_array
 
 
 if __name__ == '__main__':
 
     test_data_set = mnist_input_data.read_data_sets(input_dir).test
-    num_tests = 20
+    num_tests = 1
     for idx in range(num_tests):
         image, label = test_data_set.next_batch(1)
+        # print(image[0])
+        # arr = np.asanyarray(image[0]).reshape(28, 28)
+        # print(arr)
+        print("############", type(image[0][0]))
+        print("############", label[0])
         idx_num = "%05d" % int(idx)
-        trans_image(idx_num, image)
-        print("############", idx_num, label)
+        img_1 = trans_image(idx_num, image)
+
+        # print(image[0])
+        out_file = out_dir % idx_num
+        img_2 = read_image_arr(out_file)
+        print("############")
+        # print(img_2)
+        img_1 = np.array(image[0]).reshape(28, 28)
+        img_2 = np.array(img_2).reshape(28, 28)
+        print("############")
+        print(img_1[16])
+        print("############")
+        print(img_2[16])
+        is_equal = np.array_equal(image[0], img_2)
+        print("img save is equal img read :", is_equal)
--- web/minstServingHandler.py
+++ web/minstServingHandler.py
@@ -1,6 +1,5 @@
 #!/usr/local/bin/python
 # -*- coding: utf-8 -*-
-
 import os.path
 import json
 import subprocess
@@ -13,6 +12,11 @@ import tornado.options
 import tornado.web
 import web.base as webBase
 import logging
+import numpy as np
+from PIL import Image
+
+work_dir = "/data/stock/tf/minst_serving/input_data"
+out_dir = "/static/img/minst_serving/%s.bmp"
 
 
 # 获得页面数据。
@@ -20,12 +24,141 @@ class GetMinstServingHtmlHandler(webBase.BaseHandler):
     @gen.coroutine
     def get(self):
         # print self.uri_
-        self.render("minst_serving.html")
+        arr = np.arange(30)
+        image_array = []
+        for idx in arr:
+            out_file = out_dir % ("%05d" % idx)
+            print(out_file)
+            image_array.append(out_file)
+        self.render("minst_serving.html", image_array=image_array)
 
 
 # 获得股票数据内容。
 class GetPredictionDataHandler(webBase.BaseHandler):
     def get(self):
         # 获得分页参数。
-        start_param = self.get_argument("start", default=0, strip=False)
-        print(start_param)
+        img_url = self.get_argument("img_url", default=0, strip=False)
+        print(img_url)
+        server = "127.0.0.1:8500"
+        prediction = do_inference(server, img_url)
+        print('######### prediction : ', prediction)
+        self.write(json.dumps(prediction))
+
+
+import sys
+
+from grpc.beta import implementations
+import numpy
+import tensorflow as tf
+
+from tensorflow_serving.apis import predict_pb2
+from tensorflow_serving.apis import prediction_service_pb2
+import threading
+
+
+class _ResultCounter(object):
+    """Counter for the prediction results."""
+
+    def __init__(self, num_tests, concurrency):
+        self._num_tests = num_tests
+        self._concurrency = concurrency
+        self._result = 0
+        self._done = 0
+        self._active = 0
+        self._condition = threading.Condition()
+
+    def set_result(self, result):
+        with self._condition:
+            self._done += 1
+            self._result = result
+
+    def dec_active(self):
+        with self._condition:
+            self._active -= 1
+            self._condition.notify()
+
+    def get_error_rate(self):
+        with self._condition:
+            while self._done != self._num_tests:
+                self._condition.wait()
+            return self._result
+
+    def throttle(self):
+        with self._condition:
+            while self._active == self._concurrency:
+                self._condition.wait()
+            self._active += 1
+
+
+def _create_rpc_callback(label, result_counter):
+    """Creates RPC callback function.
+
+    Args:
+      label: The correct label for the predicted example.
+      result_counter: Counter for the prediction result.
+    Returns:
+      The callback function.
+    """
+
+    def _callback(result_future):
+        """Callback function.
+
+        Calculates the statistics for the prediction result.
+
+        Args:
+          result_future: Result future of the RPC.
+        """
+        exception = result_future.exception()
+        if exception:
+            print(exception)
+        else:
+            sys.stdout.write('.')
+            sys.stdout.flush()
+            response = numpy.array(
+                result_future.result().outputs['scores'].float_val)
+            # print("response:\n",result_future.result())
+            result = numpy.argmax(response)  # 返回prediction。
+            result_counter.set_result(result)
+            print("response:\n", result_future.result())
+            print("prediction:\n", result)
+        result_counter.dec_active()
+
+    return _callback
+
+
+# 调用 grpc 代码，将图片转换成数组，让后放到 grpc 调用。
+def do_inference(hostport, img_file):
+    host, port = hostport.split(':')
+    print(host, port)
+
+    # 创建 python grpc 代码调用。
+    channel = implementations.insecure_channel(host, int(port))
+    stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
+    request = predict_pb2.PredictRequest()
+    request.model_spec.name = 'mnist'
+    request.model_spec.signature_name = 'predict_images'
+    # image, label = test_data_set.next_batch(1)
+    label = np.array([1], dtype=np.uint8)
+
+    result_counter = _ResultCounter(1, 1)
+    img = Image.open("/data/stock/web" + img_file)
+
+    img_array = np.array(img, dtype=np.float32)
+    img_array = img_array.reshape(img_array.size)
+
+    # 新方法。很接近原始数据。
+    img_new_arr = np.divide(np.mod(img_array, 255), 255.0)
+    # img_new_arr = np.divide(np.subtract(255.0, img_array), 255.0)
+    # print(img_new_arr)
+
+    print("############", type(img_new_arr[0]))
+    print("############", type(label[0]))
+    result_counter.throttle()
+    print("############ throttle ")
+    request.inputs['images'].CopyFrom(
+        tf.contrib.util.make_tensor_proto(img_new_arr, shape=[1, img_new_arr.size]))
+    result_future = stub.Predict.future(request, 5.0)  # 5 seconds
+    result_future.add_done_callback(
+        _create_rpc_callback(label[0], result_counter))
+    print("############")
+    return result_counter.get_error_rate()
