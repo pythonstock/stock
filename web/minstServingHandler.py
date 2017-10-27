04d0a9bbfc085d244ee5abed1e4591202a8bd4cb
yhy <yhy>
2017-10-27 13:59:30 +0800

    修改 代码，直接返回数据。

--- web/minstServingHandler.py
+++ web/minstServingHandler.py
@@ -39,7 +39,7 @@ class GetPredictionDataHandler(webBase.BaseHandler):
         # 获得分页参数。
         img_url = self.get_argument("img_url", default=0, strip=False)
         print(img_url)
-        server = "127.0.0.1:8500"
+        server = "0.0.0.0:8500"
         prediction = do_inference(server, img_url)
         print('######### prediction : ', prediction)
         self.write(json.dumps(prediction))
@@ -50,97 +50,27 @@ import sys
 from grpc.beta import implementations
 import numpy
 import tensorflow as tf
-
 from tensorflow_serving.apis import predict_pb2
 from tensorflow_serving.apis import prediction_service_pb2
 import threading
 
+hostport = "0.0.0.0:8500"
+host, port = hostport.split(':')
+print(host, port)
 
-class _ResultCounter(object):
-    """Counter for the prediction results."""
-
-    def __init__(self, num_tests, concurrency):
-        self._num_tests = num_tests
-        self._concurrency = concurrency
-        self._result = 0
-        self._done = 0
-        self._active = 0
-        self._condition = threading.Condition()
-
-    def set_result(self, result):
-        with self._condition:
-            self._done += 1
-            self._result = result
-
-    def dec_active(self):
-        with self._condition:
-            self._active -= 1
-            self._condition.notify()
-
-    def get_error_rate(self):
-        with self._condition:
-            while self._done != self._num_tests:
-                self._condition.wait()
-            return self._result
-
-    def throttle(self):
-        with self._condition:
-            while self._active == self._concurrency:
-                self._condition.wait()
-            self._active += 1
-
-
-def _create_rpc_callback(label, result_counter):
-    """Creates RPC callback function.
-
-    Args:
-      label: The correct label for the predicted example.
-      result_counter: Counter for the prediction result.
-    Returns:
-      The callback function.
-    """
-
-    def _callback(result_future):
-        """Callback function.
-
-        Calculates the statistics for the prediction result.
-
-        Args:
-          result_future: Result future of the RPC.
-        """
-        exception = result_future.exception()
-        if exception:
-            print(exception)
-        else:
-            sys.stdout.write('.')
-            sys.stdout.flush()
-            response = numpy.array(
-                result_future.result().outputs['scores'].float_val)
-            # print("response:\n",result_future.result())
-            result = numpy.argmax(response)  # 返回prediction。
-            result_counter.set_result(result)
-            print("response:\n", result_future.result())
-            print("prediction:\n", result)
-        result_counter.dec_active()
-
-    return _callback
+# 创建 python grpc 代码调用。全局变量。不能每次都创建。
+channel = implementations.insecure_channel(host, int(port))
+stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
 
 
 # 调用 grpc 代码，将图片转换成数组，让后放到 grpc 调用。
 def do_inference(hostport, img_file):
-    host, port = hostport.split(':')
-    print(host, port)
-
-    # 创建 python grpc 代码调用。
-    channel = implementations.insecure_channel(host, int(port))
-    stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
     request = predict_pb2.PredictRequest()
     request.model_spec.name = 'mnist'
     request.model_spec.signature_name = 'predict_images'
     # image, label = test_data_set.next_batch(1)
     label = np.array([1], dtype=np.uint8)
 
-    result_counter = _ResultCounter(1, 1)
     img = Image.open("/data/stock/web" + img_file)
 
     img_array = np.array(img, dtype=np.float32)
@@ -153,12 +83,23 @@ def do_inference(hostport, img_file):
 
     print("############", type(img_new_arr[0]))
     print("############", type(label[0]))
-    result_counter.throttle()
     print("############ throttle ")
+    # result_counter.throttle()
     request.inputs['images'].CopyFrom(
         tf.contrib.util.make_tensor_proto(img_new_arr, shape=[1, img_new_arr.size]))
-    result_future = stub.Predict.future(request, 5.0)  # 5 seconds
-    result_future.add_done_callback(
-        _create_rpc_callback(label[0], result_counter))
-    print("############")
-    return result_counter.get_error_rate()
+    result_future = stub.Predict.future(request, 1.0)  # 5 seconds
+
+    prediction = ""
+    exception = result_future.exception()
+    if exception:
+        prediction = str(exception)
+        print(exception)
+    else:
+        sys.stdout.write('.')
+        sys.stdout.flush()
+        response = numpy.array(
+            result_future.result().outputs['scores'].float_val)
+        # print("response:\n",result_future.result())
+        prediction = str(numpy.argmax(response))
+    print("############", prediction)
+    return prediction
