2a29e4c0f9f817627a7e692af553bbd7587b1ae1
yhy <yhy>
2018-01-02 21:01:09 +0800

    增加 卖出。

--- jobs/guess_indicators_lite_sell_daily_job.py
+++ jobs/guess_indicators_lite_sell_daily_job.py
@@ -8,78 +8,10 @@ import numpy as np
 import math
 import datetime
 import heapq
+import stockstats
 
 
-### 对每日指标数据，进行筛选。将符合条件的。二次筛选出来。
-def stat_all_lite(tmp_datetime):
-    # 要操作的数据库表名称。
-    table_name = "guess_indicators_lite_buy_daily"
-    datetime_str = (tmp_datetime).strftime("%Y-%m-%d")
-    datetime_int = (tmp_datetime).strftime("%Y%m%d")
-    print("datetime_str:", datetime_str)
-    print("datetime_int:", datetime_int)
-
-    sql_1 = """
-            SELECT `date`, `code`, `name`, `changepercent`, `trade`,`turnoverratio`, `pb` ,`kdjj`,`rsi_6`,`cci`
-                        FROM stock_data.guess_indicators_lite_daily WHERE `date` = %s 
-                        and `changepercent` > 2 and `pb` > 0 and `turnoverratio` > 5
-    """
-
-    try:
-        # 删除老数据。
-        del_sql = " DELETE FROM `stock_data`.`` WHERE `date`= '%s' " % (table_name, datetime_int)
-        # common.insert(del_sql)
-        print("del_sql")
-    except Exception as e:
-        print("error :", e)
-
-    data = pd.read_sql(sql=sql_1, con=common.engine(), params=[datetime_int])
-    data = data.drop_duplicates(subset="code", keep="last")
-    print("######## len data ########:", len(data))
-    # del data["name"]
-    # print(data)
-    data["trade_float32"] = data["trade"].astype('float32', copy=True)
-    # 输入 date 用作历史数据查询。
-    stock_merge = pd.DataFrame({
-        "date": data["date"], "code": data["code"], "wave_mean": data["trade"],
-        "wave_crest": data["trade"], "wave_base": data["trade"]}, index=data.index.values)
-    print(stock_merge.head(1))
-
-    stock_merge = stock_merge.apply(apply_merge, axis=1)  # , axis=1)
-    del stock_merge["date"]  # 合并前删除 date 字段。
-    # 合并数据
-    data_new = pd.merge(data, stock_merge, on=['code'], how='left')
-
-    # 使用 trade_float32 参加计算。
-    data_new = data_new[data_new["trade_float32"] > data_new["wave_base"]]  # 交易价格大于波谷价格。
-    data_new = data_new[data_new["trade_float32"] < data_new["wave_crest"]]  # 小于波峰价格
-
-    # wave_base  wave_crest  wave_mean
-    data_new["wave_base"] = data_new["wave_base"].round(2)  # 数据保留2位小数
-    data_new["wave_crest"] = data_new["wave_crest"].round(2)  # 数据保留2位小数
-    data_new["wave_mean"] = data_new["wave_mean"].round(2)  # 数据保留2位小数
-
-    data_new["up_rate"] = (data_new["trade_float32"].sub(data_new["wave_mean"])).div(data_new["wave_crest"]).mul(100)
-    data_new["up_rate"] = data_new["up_rate"].round(2)  # 数据保留2位小数
-
-    data_new["buy"] = 1
-    data_new["sell"] = 0
-    data_new["today_trade"] = data_new["trade"]
-    data_new["income"] = 0
-    # 重命名 date
-    data_new.columns.values[0] = "buy_date"
-    del data_new["trade_float32"]
-
-    try:
-        common.insert_db(data_new, table_name, False, "`code`")
-        print("insert_db")
-    except Exception as e:
-        print("error :", e)
-    # 重命名
-    del data_new["name"]
-    print(data_new)
-
-
+# code      date today_trade
 def apply_merge(tmp):
     date = tmp["date"]
     code = tmp["code"]
@@ -91,35 +23,46 @@ def apply_merge(tmp):
     # open, high, close, low, volume, price_change, p_change, ma5, ma10, ma20, v_ma5, v_ma10, v_ma20, turnover
     # 使用缓存方法。加快计算速度。
     stock = common.get_hist_data_cache(code, date_start, date_end)
+    print("########")
+    # print(stock.tail(1))
+    close = stock.tail(1)["close"].values[0]
+    print("close:  ", close)
+    print("########")
+    return list([code, date, close])
 
-    stock = pd.DataFrame({"close": stock["close"]}, index=stock.index.values)
-    stock = stock.sort_index(0)  # 将数据按照日期排序下。
 
-    # print(stock.head(10))
-    arr = pd.Series(stock["close"].values)
-    # print(df_arr)
-    wave_mean = arr.mean()
-    max_point = 3  # 获得最高的几个采样点。
-    # 计算股票的波峰值。
-    wave_crest = heapq.nlargest(max_point, enumerate(arr), key=lambda x: x[1])
-    wave_crest_mean = pd.DataFrame(wave_crest).mean()
+# code      date  sell  sell_cci  sell_kdjj  sell_rsi_6
+def apply_merge_sell(tmp):
+    date = tmp["date"]
+    code = tmp["code"]
+    date_end = datetime.datetime.strptime(date, "%Y%m%d")
+    date_start = (date_end + datetime.timedelta(days=-300)).strftime("%Y-%m-%d")
+    date_end = date_end.strftime("%Y-%m-%d")
+    print(code, date_start, date_end)
 
-    # 输出元祖第一个元素是index，第二元素是比较的数值 计算数据的波谷值
-    wave_base = heapq.nsmallest(max_point, enumerate(arr), key=lambda x: x[1])
-    wave_base_mean = pd.DataFrame(wave_base).mean()
-    # 输出数据
-    print("##############", len(stock))
-    if len(stock) > 180:
-        #     code      date wave_base wave_crest wave_mean 顺序必须一致。返回的是行数据，然后填充。
-        return list([code, date, wave_base_mean[1], wave_crest_mean[1], wave_mean])
+    # open, high, close, low, volume, price_change, p_change, ma5, ma10, ma20, v_ma5, v_ma10, v_ma20, turnover
+    # 使用缓存方法。加快计算速度。
+    stock = common.get_hist_data_cache(code, date_start, date_end)
+    print("########")
+    # J大于100时为超买，小于10时为超卖。
+    # 强弱指标保持高于50表示为强势市场，反之低于50表示为弱势市场。
+    # 1、当CCI指标从下向上突破﹢100线而进入非常态区间时，表明股价脱离常态而进入异常波动阶段，
+    # 2、当CCI指标从上向下突破﹣100线而进入另一个非常态区间时，表明股价的盘整阶段已经结束，
+    stockStat = stockstats.StockDataFrame.retype(stock)
+    kdjj = int(stockStat["kdjj"].tail(1).values[0])
+    rsi_6 = int(stockStat["rsi_6"].tail(1).values[0])
+    cci = int(stockStat["cci"].tail(1).values[0])
+    print("kdjj:", kdjj, "rsi_6:", rsi_6, "cci:", cci)
+    if kdjj <= 10 and rsi_6 <= 50 and cci <= 100:
+        return list([code, date, 1, cci, kdjj, rsi_6])
     else:
-        return list([code, date, 0, 0, 0])
+        return list([code, date, 0, cci, kdjj, rsi_6])
 
 
 # 增加 收益计算。
 def stat_index_calculate(tmp_datetime):
     # 要操作的数据库表名称。
-    table_name = "guess_indicators_lite_buy_daily"
+    table_name = "guess_indicators_lite_sell_daily"
     datetime_str = (tmp_datetime).strftime("%Y-%m-%d")
     datetime_int = (tmp_datetime).strftime("%Y%m%d")
     print("datetime_str:", datetime_str)
@@ -127,23 +70,59 @@ def stat_index_calculate(tmp_datetime):
 
     sql_1 = """ 
                 SELECT `buy_date`, `code`, `name`, `changepercent`, `trade`, `turnoverratio`, `pb`, `kdjj`, `rsi_6`, 
-                `cci`, `wave_base`, `wave_crest`, `wave_mean`, `up_rate`, `buy`, `sell`, `today_trade`, `income` 
-                FROM """ + table_name
+                `cci`, `wave_base`, `wave_crest`, `wave_mean`, `up_rate`
+                FROM guess_indicators_lite_buy_daily """
     print(sql_1)
     data = pd.read_sql(sql=sql_1, con=common.engine(), params=[])
     data = data.drop_duplicates(subset="code", keep="last")
     print(data["trade"])
     data["trade_float32"] = data["trade"].astype('float32', copy=False)
     print(len(data))
+    data["date"] = datetime_int
 
+    stock_merge = pd.DataFrame({
+        "date": data["date"], "code": data["code"], "today_trade": data["trade"]}, index=data.index.values)
+    print(stock_merge.head(1))
+
+    stock_merge = stock_merge.apply(apply_merge, axis=1)  # , axis=1)
+
+    del stock_merge["date"]  # 合并前删除 date 字段。
+    # 合并数据
+    data_new = pd.merge(data, stock_merge, on=['code'], how='left')
+    data_new["income"] = (data_new["today_trade"] - data_new["trade_float32"]) * 100
+    data_new["buy"] = 0
+
+    # 增加售出列。看看是否需要卖出。
+    stock_sell_merge = pd.DataFrame({
+        "date": data["date"], "code": data["code"], "sell": 0, "sell_kdjj": 0, "sell_rsi_6": 0, "sell_cci": 0},
+        index=data.index.values)
+    print(stock_sell_merge.head(1))
+
+    merge_sell_data = stock_sell_merge.apply(apply_merge_sell, axis=1)  # , axis=1)
     # 重命名
-    del data["name"]
-    print(data)
+    del merge_sell_data["date"]  # 合并前删除 date 字段。
+    # 合并数据
+    data_new = pd.merge(data_new, merge_sell_data, on=['code'], how='left')
+
+    # 删除老数据。
+    try:
+        del_sql = " DELETE FROM `stock_data`.`" + table_name + "` WHERE `date`= '%s' " % datetime_int
+        common.insert(del_sql)
+        print("insert_db")
+    except Exception as e:
+        print("error :", e)
+    del data_new["trade_float32"]
+    try:
+        common.insert_db(data_new, table_name, False, "`code`")
+        print("insert_db")
+    except Exception as e:
+        print("error :", e)
+    # 重命名
+    del data_new["name"]
+    print(data_new)
 
 
 # main函数入口
 if __name__ == '__main__':
-    # 二次筛选数据。
-    # tmp_datetime = common.run_with_args(stat_all_lite)
     # 计算买卖。
     tmp_datetime = common.run_with_args(stat_index_calculate)
