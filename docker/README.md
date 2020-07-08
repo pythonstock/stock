074f058d07309573ba24d61ad37f1aca11600de5
“yhy” <yhy@com>
2020-07-08 10:23:51 +0800

    增加 docker 镜像安装，拆分基础镜像。和应用镜像。

---
+++ docker/README.md
@@ -0,0 +1,20 @@
+
+# python 基础镜像
+
+基础镜像升级到 2020年7月的版本
+
+保证运行的最少基础环境，基础环境使用python3.6的版本。安装了超级多的lib库。非常的好用。
+
+mysqlclient
+sqlalchemy
+requests
+numpy
+tushare
+tornado torndb
+bokeh
+stockstats
+ta-lib
+jupyter
+sklearn
+
+# 
---
+++ docker/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+
+NOW_MONTH=$(date "+%Y-%m")
+
+DOCKER_TAG=pythonstock/pythonstock:base-${NOW_MONTH}
+
+echo " docker build -f Dockerfile -t ${DOCKER_TAG} ."
+#docker build -f Dockerfile -t ${DOCKER_TAG} .
+echo "#################################################################"
+echo " docker push ${DOCKER_TAG} "
+
+
