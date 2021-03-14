ea593e8b1392cf338ecac72325b16b685974d1c6
Zhimin Sun <121097854@qq.com>
2021-03-14 10:33:01 +0800

    Update daily_job.py

--- jobs/daily_job.py
+++ jobs/daily_job.py
@@ -29,7 +29,7 @@ def stat_all(tmp_datetime):
     print("datetime_int:", datetime_int)
     # 增加pro_api，token需要用户自己注册
     pro = ts.pro_api(common.get_tushare_token())
-    data = pro.top_list(trade_date=datetime_str)
+    data = pro.top_list(trade_date=datetime_int)
     # 处理重复数据，保存最新一条数据。最后一步处理，否则concat有问题。
     #
     if not data is None and len(data) > 0:
