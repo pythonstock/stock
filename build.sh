2aa29ae48c4f55e139552c7c228f779c03dd47b8
gao <gaohuadong2001@gmail.com>
2022-01-17 21:42:20 +0800

    取消jupyter密码

--- build.sh
+++ build.sh
@@ -2,13 +2,10 @@
 
 DOCKER_TAG=pythonstock/pythonstock:latest
 
-sudo rm -rf data
 sudo rm -f jobs/nohup.out
 
 echo " docker build -f Dockerfile -t ${DOCKER_TAG} ."
-docker build -f Dockerfile -t ${DOCKER_TAG} .
+sudo docker build -f Dockerfile -t ${DOCKER_TAG} .
 echo "#################################################################"
 echo " docker push ${DOCKER_TAG} "
-
-mkdir data
 
--- jobs/run_jupyter.sh
+++ jobs/run_jupyter.sh
@@ -2,5 +2,7 @@
 
 mkdir -p /data/notebooks
 
+mkdir -p ~/.jupyter
+echo "c.NotebookApp.token = ''"  > ~/.jupyter/jupyter_notebook_config.py
 /usr/local/bin/jupyter notebook --NotebookApp.notebook_dir='/data/notebooks'  --ip=0.0.0.0 \
     --allow-root >> /data/logs/jupyter-notebook.log
