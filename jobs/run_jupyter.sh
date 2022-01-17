6676c6cd8804ded904ec8dead0d51bc6b277d602
gao <gaohuadong2001@gmail.com>
2022-01-17 22:10:43 +0800

    无

--- jobs/run_jupyter.sh
+++ jobs/run_jupyter.sh
@@ -2,7 +2,5 @@
 
 mkdir -p /data/notebooks
 
-mkdir -p ~/.jupyter
-echo "c.NotebookApp.token = ''"  > ~/.jupyter/jupyter_notebook_config.py
 /usr/local/bin/jupyter notebook --NotebookApp.notebook_dir='/data/notebooks'  --ip=0.0.0.0 \
-    --allow-root >> /data/logs/jupyter-notebook.log
+	--NotebookApp.token='' --allow-root >> /data/logs/jupyter-notebook.log
